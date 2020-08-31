4db73f4a9b541d385060c363edad7261a975f55f
Mauricio Antúnez <5807540+MauricioAntunez@users.noreply.github.com>
2020-08-31 08:57:41 -0400

    React spectrum como Design System

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  Lazy,
+  Suspense
+} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [datos, setDatos] = useState(false),
+        [primeracarga, setPrimeracarga] = useState(true),
+        [resultadobusqueda, setResultadobusqueda] = useState([]),
+        [filtroresultado, setFiltroresultado] = useState({
+          tipoProducto:null,
+          tipoCanasta:null,
+          tipoBases:null,
+          inicioAbasteimiento:null
+        }),
+      consultarTSV = async (urlCsv) => {
+        const tsvdata = await fetch(urlCsv),
+          info = await tsvdata.text().catch(error => {
+            console.error(error)
+          })
+        let listProductos = []
+
+        try {
+          let infotsvList = info.split("\n")
+
+          //Código producto Descripción del producto	Descripción completa del producto	Tipo de producto	Tipo de canasta	Fecha inicio abastecimiento	Tipo de Bases
+          for (let index = 1; index < infotsvList.length; index++) {
+            const element = infotsvList[index].split("\t");
+            listProductos.push({
+              codigo:element[0],
+              descCorta:element[1],
+              descLarga:element[2],
+              tipoProducto:element[3],
+              tipoCanasta:element[4],
+              inicioAbastecimiento:element[5],
+              tipoBases:element[6]
+            })
+          }
+          setDatos(listProductos)
+        } catch (e) {
+          console.error(e)
+        }
+      },
+      wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+      useEffect(() => {
+        switch (primeracarga) {
+            case true:
+                consultarTSV('./data/Listado.tsv')
+                setPrimeracarga(false)
+                break;
+                
+
+            case false:
+                console.log(datos)
+                break;
+        
+            default:
+                break;
+        }
+        
+    },[datos,filtroresultado])
+
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense>
+        <Resultado data={data} />
+      </Suspense>
     </div>
   );
 }
