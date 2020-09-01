ed412f21a187882b771e4c1b1b45c8feb9b1ebca
Mauricio Antúnez <5807540+MauricioAntunez@users.noreply.github.com>
2020-09-01 10:16:39 -0400

    Updates

---
+++ precache-manifest.29c699f23e3901c8c633d246365fd7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f1ae61b657766903ab0185f871d295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f99231fba59944145b33",
+    "url": "/static/css/2.da92a648.chunk.css"
+  },
+  {
+    "revision": "df1bd07bf7447a7860d7",
+    "url": "/static/css/main.9fdbc95f.chunk.css"
+  },
+  {
+    "revision": "f99231fba59944145b33",
+    "url": "/static/js/2.8e0fd53c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e0fd53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1bd07bf7447a7860d7",
+    "url": "/static/js/main.57889392.chunk.js"
+  },
+  {
+    "revision": "661d6f3c70344f157fb4",
+    "url": "/static/js/runtime-main.8ed29063.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/buscador-productos-cenabast/precache-manifest.2c32298022d3e6197b5a81df988f8512.js"
+  "/precache-manifest.29c699f23e3901c8c633d246365fd7b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buscador-productos-cenabast/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
