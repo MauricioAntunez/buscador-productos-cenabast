f6cdb2359800e7d2ecf8538cfb708adbef597607
Mauricio Antúnez <5807540+MauricioAntunez@users.noreply.github.com>
2020-09-01 10:17:08 -0400

    Updates

---
+++ precache-manifest.2843ed996e29ea72aa05d369b95a2366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a57c57dae18594a800a4d97b60c300e",
+    "url": "/buscador-productos-cenabast/index.html"
+  },
+  {
+    "revision": "f99231fba59944145b33",
+    "url": "/buscador-productos-cenabast/static/css/2.da92a648.chunk.css"
+  },
+  {
+    "revision": "52112025660ed47329d7",
+    "url": "/buscador-productos-cenabast/static/css/main.9fdbc95f.chunk.css"
+  },
+  {
+    "revision": "f99231fba59944145b33",
+    "url": "/buscador-productos-cenabast/static/js/2.8e0fd53c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscador-productos-cenabast/static/js/2.8e0fd53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52112025660ed47329d7",
+    "url": "/buscador-productos-cenabast/static/js/main.6d3c754a.chunk.js"
+  },
+  {
+    "revision": "fbb7f3c79506dd2f99f4",
+    "url": "/buscador-productos-cenabast/static/js/runtime-main.60ab7b15.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29c699f23e3901c8c633d246365fd7b9.js"
+  "/buscador-productos-cenabast/precache-manifest.2843ed996e29ea72aa05d369b95a2366.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buscador-productos-cenabast/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
